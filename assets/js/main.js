0a5ba2c759573d8e8577a705d5199176cbe65215
Ignacio Camillucci <ignacio.camillucci@gmail.com>
2023-04-05 20:01:47 -0300

    update
    + funcion para enviar obj album a la api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-function saveAlbum(){
+ async function saveAlbum(){
+
     // capturo el valor de los dos input
     const userId = document.getElementById("userId").value;
     const title = document.getElementById("title").value;
@@ -7,13 +8,34 @@ function saveAlbum(){
     if(userId == 0 || title == ""){
         return alert("Los campos solcitados no pueden ser nulos")
     }
+    
     // POST 
-    // https://jsonplaceholder.typicode.com/posts
+    // https://jsonplaceholder.typicode.com/albums
     // {
     //     "userId" : 3,
     //     "title": "Probando"
     // }
+   
+    // crearmos y convertimos a json nuestro obj
 
-    console.log("Funciona")
+   const data = {"userId":userId, "title":title}
+   const dataJson = JSON.stringify(data)
+
+   // armamos nuestro request 
+   const response = await fetch('https://jsonplaceholder.typicode.com/albums',{
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body:dataJson
+   })
+
+   const responseData = await response.json()
+
+   document.getElementById("respuesta").innerHTML = `
+   <h2>Se creo correctamente el album</h2> 
+  <p>id: ${responseData.id} y su titulo es: ${responseData.title}</p>`
+
+  document.getElementById("formularioAlbum").reset(); 
 
 }
